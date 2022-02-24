ae89b2917b17ec949d336c3be89c24130b98dd87
esciafardini <edward.ciafardini@aclaimant.com>
2022-02-23 22:30:21 -0500

    more keymaps, less lsp

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -21,14 +21,25 @@ vim.g.maplocalleader = ","
 
 -- Normal --
 vim.cmd [[
-let @k = "f(v%xh"
-let @b = "F)v%xh"
-let @c = "v%y"
 let @y = "v%y"
 let @p = "v%p"
+let @x = "v%x"
 let @e = ",ee"
 let @r = ",er"
 let @w = ",ew"
+let @b = ",eb"
+let @v = ",lv"
+let @z = ",ece"
+let @k = ",ecr"
+
+"Debug Tools
+let @s = ",w:€kblog/spy("
+let @d = ",wlog/daff zz("
+let @t = 'i(aclaimant.services.core.db.customer-schema.fns/transaction-in-schema "lli (llhhhhiq€kbl'
+let @g = ",[&h%hi :as argsll%"
+let @u = "lll kri€kb€kb€kb€kb€kb€kb€kb€kb€kbl%"
+
+let @a='"Vim''s quote handling is a little tricky"'
 
 let g:loaded_python_provider = 0
 let g:loaded_perl_provider = 0
@@ -42,21 +53,25 @@ keymap("n", "<Space>3", "<C-w>v", opts)
 keymap("n", "<Space>o", "<C-w>w", opts)
 keymap("n", "<Space>j", "kJ", opts)
 keymap("n", "<Space>r", "dv", opts)
-keymap("n", "<Space>,", "F(", opts)
-keymap("n", "<Space>.", "f)", opts)
 --More Clojure
+keymap("n", "<Space>5", "@5", opts)
+keymap("n", "<Space>7", "@7", opts)
+keymap("n", "<Space>8", "@8", opts)
+keymap("n", "<Space>9", "@9", opts)
 keymap("n", "<Space>e", "@e", opts)
 keymap("n", "<Space>r", "@r", opts)
 keymap("n", "<Space>w", "@w", opts)
 keymap("n", "<Space>ks", "@k", opts)
 keymap("n", "<Space>kb", "@b", opts)
+keymap("n", "<Space>yy", "@y", opts)
+keymap("n", "<Space>yp", "@p", opts)
 
 -- Navigate Windows and Buffers
-keymap("n", "<Space>-", ":bnext<CR>", opts)
-keymap("n", "<Space>=", ":bprevious<CR>", opts)
+keymap("n", "<Space>+", ":bnext<CR>", opts)
+keymap("n", "<Space>=", ":bnext<CR>", opts)
+keymap("n", "<Space>-", ":bprevious<CR>", opts)
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-n>", "<C-w>j", opts)
-keymap("n", "<C-i>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
