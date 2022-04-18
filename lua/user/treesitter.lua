195cca44cbead71bad06d64ad5c140fd1bb5ba1e
esciafardini <edward.ciafardini@aclaimant.com>
2022-04-17 20:33:50 -0400

    Keeping up with warning messages, whew

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,8 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = { "clojure", "css", "bash", "c", "dockerfile", "html", 
+    "javascript", "json", "lua", "python", "scss", "vim", "yaml" }, 
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
